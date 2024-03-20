40e475d2a83bf2a10f34f56d56df328834578e46
lyrahgames <lyrahgames@mailbox.org>
2024-03-20 09:33:52 +0100

    Add proper smoke test for `libpmp`

--- libpmp/tests/basics/driver.cpp
+++ libpmp/tests/basics/driver.cpp
@@ -1,11 +1,16 @@
 #include <pmp/surface_mesh.h>
 
+// Needs to be provided to gain access
+// to 'read' and 'write' functions.
+//
+#include <pmp/io/io.h>
+
 #undef NDEBUG
 #include <cassert>
 
 int main() {
-  // pmp::SurfaceMesh mesh;
-  // pmp::read(mesh,"input.obj");
-  // // .. do awesome things with your mesh
-  // pmp::write(mesh,"output.obj");
+  pmp::SurfaceMesh mesh;
+  pmp::read(mesh, "input.obj");
+  // .. do awesome things with your mesh
+  pmp::write(mesh, "output.obj");
 }
